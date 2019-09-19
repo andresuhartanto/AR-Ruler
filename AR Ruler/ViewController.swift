4154f84426635b76f0d05f5b8c1918816410dee5
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-09-19 17:09:03 +0800

    Add remove dot function

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,6 +13,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -44,6 +45,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            
+            dotNodes = [SCNNode]()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint)
             
@@ -87,10 +96,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func updateText(text : String, atPosition position: SCNVector3) {
+        
+        textNode.removeFromParentNode()
+        
         let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
         textGeometry.firstMaterial?.diffuse.contents = UIColor.red
         
-        let textNode = SCNNode(geometry: textGeometry)
+        textNode = SCNNode(geometry: textGeometry)
         
         textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
         
