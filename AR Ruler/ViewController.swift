0ed318757dd304ad968d3fb53eb61d5721f775d4
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-09-19 16:27:33 +0800

    First commit

---
+++ AR Ruler/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  AR Ruler
+//
+//  Created by Andre Suhartanto on 19/09/2019.
+//  Copyright © 2019 Andre Suhartanto. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
